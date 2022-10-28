732a1204dc8d6f1576188151a27c9b97e017a504
Andrei Malashkin <malashkin.andrey@gmail.com>
2022-10-28 13:04:11 +0000

    Initial

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 
 #include "rapidjson/error/en.h"
 
+#include "../3rd_party/TLV/cpp/tlv_box.h"
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -12,13 +14,45 @@
 
 using namespace rapidjson;
 
-void writeToFile(const GenericDocument<ASCII<>>& record)
+enum TlvTypesCode
+{
+    BOOL,
+    INT,
+    STRING
+};
+
+void writeJsonToFile(const GenericDocument<ASCII<>>& record)
 {
     std::ofstream ofs("output.json", std::ios::app);
     OStreamWrapper osw(ofs);
 
     Writer<OStreamWrapper> writer(osw);
     record.Accept(writer);
+}
+
+void writeTlvToFile(const GenericDocument<ASCII<>>& record)
+{
+    tlv::TlvBox box;
+    for (auto iter = record.MemberBegin(); iter != record.MemberEnd(); ++iter)
+    {
+        const auto* key = iter->name.GetString();
+        int keyNum = std::stoi(key);
+        box.PutBoolValue(TlvTypesCode::INT, keyNum);
+
+        if(iter->value.IsBool())
+            box.PutBoolValue(TlvTypesCode::BOOL, iter->value.GetBool());
+        else if(iter->value.IsInt())
+            box.PutBoolValue(TlvTypesCode::INT, iter->value.GetInt());
+        if(iter->value.IsString())
+            box.PutBoolValue(TlvTypesCode::STRING, iter->value.GetString());
+    }
+
+    if (!box.Serialize())
+    {
+        std::cout << "box Serialize Failed!\n";
+        return;
+    }
+    std::cout << "box Serialize Success, " << box.GetSerializedBytes() << " bytes \n";
 }
 
 int main(int argc, char *argv[])
@@ -39,15 +73,16 @@ int main(int argc, char *argv[])
     std::map<std::string, int> keyDict;
     auto findOrCreateKeyRecord = [&keyDict](const std::string& key) -> int
     {
-        auto keyPoition = keyDict.find(key);
-        if(keyPoition == keyDict.end())
+        auto keyPosition = keyDict.find(key);
+        if(keyPosition == keyDict.end())
         {
             keyDict[key] = keyDict.size();
             return keyDict.size() - 1;
         }
-        return keyPoition->second;
+        return keyPosition->second;
     };
 
+    // Write records:
     int lineNumber = 0;
     while (std::getline(*inputSource, line))
     {
@@ -66,7 +101,8 @@ int main(int argc, char *argv[])
             std::string keyCode = std::to_string(keyIndex);
             iter->name.SetString(keyCode.c_str(), keyCode.size(), inputRecord.GetAllocator());
         }
-        writeToFile(inputRecord);
+        writeJsonToFile(inputRecord);
+        writeTlvToFile(inputRecord);
         ++lineNumber;
     }
     return 0;
