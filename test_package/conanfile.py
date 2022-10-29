706bb81f817ef952819cb73347816bc9f1ccf1e7
andrei malashkin <andrei.malashkin@reactivereality.com>
2022-10-29 13:23:48 +0200

    add test data

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -2,7 +2,8 @@ import os
 from conan import ConanFile
 from conan.tools.build import cross_building
 from conan.tools.layout import basic_layout
-
+import numpy as np
+import filecmp
 
 class JsonDictionaryParserTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
@@ -17,12 +18,24 @@ class JsonDictionaryParserTestConan(ConanFile):
 
     def layout(self):
         basic_layout(self)
+        
+    def _run_test_case(self, test_name):
+        input = "../{}.tlv".format(test_name)
+        output = "{}.tlv".format(test_name)
+        
+        self.run("json_dictionary_parser {} {}".format(input, output), env="conanrun")
+        same = filecmp.cmp(input, output, shallow=False)
+        if same:
+            print("Files are different, test {} failed".format(test_name), file=sys.stderr)
+        else:
+            print("Test {} succeeded".format(test_name))
 
     def test(self):
         if cross_building(self):
             return
 
         self.run("json_dictionary_parser help", env="conanrun")
-        self.run("json_dictionary_parser ../test1_empty_input.json test1.output", env="conanrun")
-        self.run("json_dictionary_parser ../test2_task_input.json  test2.output", env="conanrun")
-        self.run("json_dictionary_parser ../test3_one_record.json  test3.output", env="conanrun")
+
+        self._run_test_case("test1_empty_input")
+        self._run_test_case("test2_task_input")
+        self._run_test_case("test3_one_record")
